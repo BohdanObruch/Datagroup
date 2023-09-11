593d520dd9a7f45b8e211dab84342a627238c1bb
BohdanObruch <bodan1992@gmail.com>
2023-09-11 07:39:24 +0300

    made changes to running tests

--- datagroup_tests/model/notifications_block.py
+++ datagroup_tests/model/notifications_block.py
@@ -5,5 +5,7 @@ from datagroup_tests.model.screenshot import creating_and_saving_a_screenshot
 
 
 def checking_the_display_of_the_notifications_block():
-    browser.element(alert_block).with_(timeout=10).should(be.visible)
-    creating_and_saving_a_screenshot()
+    if browser.element(alert_block).with_(timeout=10).should(be.not_.visible):
+        pass
+    else:
+        creating_and_saving_a_screenshot()
