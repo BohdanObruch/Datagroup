b6804635ec18e13a754811e92f5d624d6b24e177
BohdanObruch <bodan1992@gmail.com>
2023-09-14 19:37:22 +0300

    added the pytz library to display the exact time (in Ukraine)

--- datagroup_tests/model/screenshot.py
+++ datagroup_tests/model/screenshot.py
@@ -1,10 +1,19 @@
 from datetime import datetime
+import pytz
 from selene import browser
 from datagroup_tests.controls.utils import resource
 
 
+def get_kiev_time():
+    kiev_timezone = pytz.timezone('Europe/Kiev')
+    kiev_time = datetime.now(kiev_timezone)
+    return kiev_time
+
+
 def creating_and_saving_a_screenshot():
-    current_datetime = datetime.now().strftime('%d.%m.%y_%H.%M')
+    current_datetime = get_kiev_time().strftime('%d.%m.%y_%H.%M')
     screenshot_filename = f'screenshot_{current_datetime}.png'
 
     browser.driver.save_screenshot(resource(screenshot_filename))
+
+
