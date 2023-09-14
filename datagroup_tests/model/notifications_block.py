b827b26bd598f980da304aff501f78108616b4c5
BohdanObruch <bodan1992@gmail.com>
2023-09-14 19:24:48 +0300

    fixed the code in the checking_the_display_of_the_notifications_block function

--- datagroup_tests/model/notifications_block.py
+++ datagroup_tests/model/notifications_block.py
@@ -11,6 +11,7 @@ def checking_the_display_of_the_notifications_block():
         if browser.element(alert_block).matching(have.text('По вашому будинку аварія послуги Інтернет')
                                             .or_(have.text('Аварія на Вашій ділянці'))):
             creating_and_saving_a_screenshot()
+            assert False, 'По вашому будинку аварія послуги Інтернет'
         else:
             pass
     elif browser.element(alert_block).with_(timeout=10).matching(be.not_.visible):
